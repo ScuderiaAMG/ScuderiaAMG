59b34bb4f095671d8dfda241f2b8f5c1967ca4f6
ScuderiaAMG <escherichia30636@qq.com>
2026-01-14 10:20:09 +0800

    update

--- C/test.c
+++ C/test.c
@@ -1,6 +1,15 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
-    printf("Hello, VS Code!\n");
+// int main() {
+//     printf("Hello, VS Code!\n");
+//     return 0;
+// }
+
+#include <stdio.h>
+int main()
+{
+    printf("%3s,%7.2s,%.4s,%-5.3s\n","HUST","HUST","HUST","HUST");
+    float x = 357.987;
+    printf("%f,%10f,%10.2f,%.2f,%-10.2f\n",x,x,x,x,x);
     return 0;
 }
