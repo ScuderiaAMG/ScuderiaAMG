664e796222cb5a6b17b565e19f2a498af8379fd7
Legion-83DG <escherichia30636@qq.com>
2026-03-29 21:20:26 +0800

    update

--- WheelRobots/controllers/my_controller/my_controller.py
+++ WheelRobots/controllers/my_controller/my_controller.py
@@ -18,58 +18,61 @@ timestep = int(robot.getBasicTimeStep())
 right_motor = robot.getDevice('RightMotor')
 left_motor = robot.getDevice('LeftMotor')
 
-# right_motor.setPosition(float('inf'))
-# left_motor.setPosition(float('inf'))
-
-# right_motor.maxVelocity(100)
-# left_motor.maxVelocity(100)
-
-# right_motor.setVelocity(0)
-# left_motor.setVelocity(0)
+# Use torque control mode.
+right_motor.setPosition(float('inf'))
+left_motor.setPosition(float('inf'))
+right_motor.setVelocity(0.0)
+left_motor.setVelocity(0.0)
 
 IMU_center = robot.getDevice('InertialUnit')
 IMU_center.enable(timestep)
-count = 1
 
 TWR_center = robot.getDevice('TWRGyro')
 TWR_center.enable(timestep)
+
+dt = timestep / 1000.0
+
+# PI gains (start values, tune in Webots).
+target_pitch = 0.0
+KP = 1.0
+KI = 0.8
+
+# Anti-windup and output limits.
+integral_error = 0.0
+integral_limit = 0.4
+torque_limit = 8.0
+
+log_interval_steps = max(1, int(200 / timestep))
+step_count = 0
+
+
+def clamp(value, min_value, max_value):
+    return max(min_value, min(value, max_value))
+
+
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-    RPY_value = IMU_center.getRollPitchYaw()
-    RAD_value = TWR_center.getValues()
-    print(RAD_value[1])
+    # Pitch is the body tilt for balancing in this model.
+    pitch = IMU_center.getRollPitchYaw()[1]
+    pitch_rate = TWR_center.getValues()[1]
 
-    # k = 0
-    # if count < 10:
-        # k = 0.2
-    # else:
-        # k = 0
- 
-    k=0
-    Kd = 0.01
-    ks = -0.8*(k-RPY_value[1]) + Kd * RAD_value[1]
-    # ks = -0.4*(0-RPY_value[1])
-    # ks = 0
-    # ks = -0.2*(k-RPY_value[1])+0.0*RAD_value[1]
-  
-  
-    right_motor.setTorque(ks)
-    left_motor.setTorque(ks)
-    
-    count = count + 1
-        
-    
-    
-    
-    
-    # Process sensor data here.
+    # Keep the same control direction as your previous script.
+    error = pitch - target_pitch
+    integral_error = clamp(integral_error + error * dt, -integral_limit, integral_limit)
 
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
+    torque_cmd = KP * error + KI * integral_error
+    torque_cmd = clamp(torque_cmd, -torque_limit, torque_limit)
+
+    right_motor.setTorque(torque_cmd)
+    left_motor.setTorque(torque_cmd)
+
+    if step_count % log_interval_steps == 0:
+        print(
+            f"pitch={pitch:.4f} rad, rate={pitch_rate:.4f} rad/s, "
+            f"integral={integral_error:.4f}, torque={torque_cmd:.3f}"
+        )
+
+    step_count += 1
 
 # Enter here exit cleanup code.
